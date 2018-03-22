7bb165ec01fef4ecf0620e88b78d4af3e9a6e595
Vadrey <l.lesniak91@gmail.com>
2018-03-22 23:03:10 +0100

    Revert "Delete App.java"
    This reverts commit eab2165108b4142f0c831aa642d102bca29bfce0.

---
+++ src/main/java/irc/irc__client/App.java
@@ -0,0 +1,32 @@
+package irc.irc__client;
+
+<<<<<<< HEAD
+import java.io.IOException;
+=======
+import irc.irc__client.utils.Parser;
+>>>>>>> feature-Czarek
+
+/**
+ * Hello world!
+ *
+ */
+
+/*
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+
+
+}
+*/
+
+public class App {
+    public static void main(String[] args) throws IOException {
+
+        LoginWindow lw = new LoginWindow();
+    }
+}
